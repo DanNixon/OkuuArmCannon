b22a9ab734a20d2bad6611c511ac4ad8b936b89c
Dan Nixon <dan@dan-nixon.com>
2015-12-05 14:27:08 +0000

    Update test

--- firmware/OkuuArmCannon/examples/HelloWorld/HelloWorld.ino
+++ firmware/OkuuArmCannon/examples/HelloWorld/HelloWorld.ino
@@ -11,25 +11,37 @@ void setup()
   cannon.begin();
   delay(1000);
 
-  cannon.setLaser(COL_R, true);
-  delay(1000);
-  cannon.setLaser(COL_G, true);
-  delay(1000);
-  cannon.setLaser(COL_B, true);
-  delay(1000);
-
-  cannon.setMidSectionLights(0, 255, 255, 255);
-  delay(1000);
-  cannon.setMidSectionLights(1, 255, 0, 0);
-  delay(1000);
-  cannon.setMidSectionLights(2, 0, 255, 0);
-  delay(1000);
-  cannon.setMidSectionLights(3, 0, 0, 255);
-  delay(1000);
-
-  cannon.setApertureLights(OkuuArmCannon::ALL_PIXELS, 255, 255, 255);
+  cannon.setLaser(COL_ALL, true);
 }
 
 void loop()
 {
+  uint16_t i, j;
+
+  for(j = 0; j < 256; j++)
+  {
+    for(i = 0; i < cannon.NUM_MID_SECTIONS; i++)
+      cannon.setMidSectionLights(i, wheel(((20*i)+j) & 255));
+    for(i = 0; i < cannon.NUM_APERTURE_PIXELS; i++)
+      cannon.setApertureLights(i, wheel(((2*i)+j) & 255));
+
+    delay(50);
+  }
+}
+
+uint32_t wheel(byte pos)
+{
+  pos = 255 - pos;
+
+  if(pos < 85)
+    return Adafruit_NeoPixel::Color(255 - pos * 3, 0, pos * 3);
+
+  if(pos < 170)
+  {
+    pos -= 85;
+    return Adafruit_NeoPixel::Color(0, pos * 3, 255 - pos * 3);
+  }
+
+  pos -= 170;
+  return Adafruit_NeoPixel::Color(pos * 3, 255 - pos * 3, 0);
 }
